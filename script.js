527bb09f86a2fba82e1c9cb12a8ab024d9e26918
Shayan Affandi <shayanaffandi5785@gmail.com>
2022-11-22 07:13:51 +0500

    completed the last function

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const body = document.body
 const container = document.querySelector("#container")
 let num = 1;
 
+const box = document.querySelector('.box')
+
+
 toggle.addEventListener('click', ()=> body.className ? body.classList.remove('toggle'): body.classList.add('toggle'))
 
 add.addEventListener('click', () => {
@@ -13,6 +16,16 @@ add.addEventListener('click', () => {
     const textNode = document.createTextNode(num++)
     div.appendChild(textNode )
     div.setAttribute('title', "I'm a box")
-    
+
+        // box.addEventListener('click', ()=>{
+        //     console.log(box);
+        // })
+
+})
+
+container.addEventListener("click", e => {
+    if (e.target.classList.contains('box')) {
+      e.target.remove();      
+    }
 })
 
